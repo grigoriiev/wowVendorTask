ebe3aad48be10adf72b6915073f226dc60e01961
Andrey <roliol27@yandex.ru>
2021-03-13 14:17:06 +0300

    second commit

--- connection.php
+++ connection.php
@@ -1,2 +1,8 @@
 <?php
-R::setup('mysql:host=127.0.0.1:3307;dbname=wowvendor', 'root', 'root');
+require_once 'vendor/autoload.php';
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+R::setup('mysql:host=127.0.0.1:3307;dbname='.$_ENV["DB_NAME"], $_ENV["DB_USER_NAME"], $_ENV["DB_USER_PASSWORD"]);
